a7c5c25ca88978f27e1eb44021dc1f529e6b71ed
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 01:23:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,15 +2,8 @@ require 'pry';
 
 def prime?(num)
   range_array = (2..(num-1).to_a 
-  primes = []
 
   return range_array.none?{|divisor| num%divisor == 0}
-    
-    if(primes.none?{|prime| num%prime == 0})
-      primes.push(num)
-    end
-  end
-  return primes.last == num
 end
 
 prime?(38)
