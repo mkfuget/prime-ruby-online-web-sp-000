e769fab15baeeade8674acfe1085006907a9ffa7
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 01:29:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 require 'pry';
 
 def prime?(num)
-  return ((num>1) && ((2..(num-1)).to_a.none?{|divisor| num%divisor == 0}))
+  return ((num>1) && ((2..((num-1)/2)).to_a.none?{|divisor| num%divisor == 0}))
 end
 
 puts prime?(29)
