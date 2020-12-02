f3c0043242c8fd34f68ea41e60c3a26e9d34f20c
Michael Fuget <michaelfuget@gmail.com>
2020-12-02 01:17:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require 'pry';
+
+def prime?(num)
+  range_array = (2..num).to_a 
+  primes = []
+  puts "before while loop"
+
+  range_array.each do |num|
+    puts "#{num}, #{(primes.none?{|prime| num%prime == 0})}"
+    puts primes
+    if(primes.none?{|prime| num%prime == 0})
+      primes.push(num)
+    end
+  end
+  puts "After while loop"
+  return primes.last == num
+end
+
+prime?(38)
